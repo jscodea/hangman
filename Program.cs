4705336f85767dfab3392b24c505a8590d65b774
jscodea <jonas.siudikas@codeacademylt.onmicrosoft.com>
2022-12-05 18:26:50 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            //IMPORT ALLOWED WORDS
+            //Console.WriteLine("Hello, World!");
+            //string[] lines = File.ReadAllLines("words.txt");
+            string[] words = { "test", "best" };
+
+            //Choosing play word randomly
+            string playWord = words[random.Next(words.Length)];
+
+            List<char> guessedCorrectLetters = new List<char>();
+            const int initialPlayerLives = 6;
+            int playerLives = initialPlayerLives;
+            char enteredLetter;
+            bool playerWon = false;
+
+            while (true)
+            {
+                Program.DrawHangman(playerLives);
+                if (playerWon)
+                {
+                    Console.WriteLine("You WON!");
+                    if(! Program.PlayAgain(ref playerLives, ref guessedCorrectLetters, ref playWord, words, initialPlayerLives, random))
+                    {
+                        break;
+                    }
+                }
+                enteredLetter = Program.EnterLetter();
+                Console.Clear();
+                playerWon = Program.ProccessWord(playWord, enteredLetter, ref guessedCorrectLetters, ref playerLives);
+                Console.WriteLine();
+                if (playerLives < 1)
+                {
+                    Console.WriteLine("You LOST!");
+                    if (!Program.PlayAgain(ref playerLives, ref guessedCorrectLetters, ref playWord, words, initialPlayerLives, random))
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        static bool ProccessWord(string playWord, 
+            char guessedLetter,
+            ref List<char> guessedLetters,
+            ref int playerLives
+         )
+        {
+            bool playerWon = true;
+            bool deductLive = true;
+            for (int i = 0; i < playWord.Length; i++)
+            {
+                if (playWord[i] == guessedLetter)
+                {
+                    guessedLetters.Add(guessedLetter);
+                    deductLive = false;
+                }
+                if (guessedLetters.Contains(playWord[i]))
+                {
+                    Console.Write($" {playWord[i]} ");
+                    continue;
+                }
+                Console.Write(" _ ");
+                playerWon = false;
+            }
+            if (deductLive)
+            {
+                playerLives--;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Lives left: {playerLives}");
+            return playerWon;
+        }
+
+        static char EnterLetter()
+        {
+            char enteredLetter;
+            while (true)
+            {
+                Console.WriteLine("Enter letter:");
+                enteredLetter = Char.ToLower(Console.ReadKey().KeyChar);
+                if (Char.IsLetter(enteredLetter))
+                {
+                    break;
+                }
+                Console.WriteLine("Wrong input!");
+            }
+
+            return enteredLetter;
+        }
+
+        static bool PlayAgain(
+            ref int playerLives,
+            ref List<char> guessedLetters,
+            ref string playWord,
+            string[] words,
+            int initialPlayerLives,
+            Random random
+            )
+        {
+            Console.WriteLine("Play again? y/n");
+            if (Char.ToLower(Console.ReadKey().KeyChar) == 'y')
+            {
+                Console.Clear();
+                playerLives = initialPlayerLives;
+                guessedLetters = new List<char>();
+                playWord = words[random.Next(words.Length)];
+                return true;
+            }
+            return false;
+        }
+
+        static void DrawHangman(int livesLeft)
+        {
+            Console.WriteLine("H A N G M A N");
+            switch (livesLeft)
+            {
+                case 5:
+                    Console.WriteLine(@"
+                        +---+
+                        O     |
+                              |
+                              |
+                             ===
+                    ");
+                    break;
+                case 4:
+                    Console.WriteLine(@"
+                        +---+
+                        O     |
+                        |     |
+                              |
+                             ===
+                    ");
+                    break;
+                case 3:
+                    Console.WriteLine(@"
+                            +---+
+                            O     |
+                           /|     |
+                                  |
+                                 ===
+                        ");
+                    break;
+                case 2:
+                    Console.WriteLine(@"
+                        +---+
+                        O     |
+                       /|\    |
+                              |
+                             ===
+                    ");
+                    break;
+                case 1:
+                    Console.WriteLine(@"
+                        +---+
+                        O     |
+                       /|\    |
+                       /      |
+                             ===
+                    ");
+                    break;
+                case 0:
+                    Console.WriteLine(@"
+                        +---+
+                        O     |
+                       /|\    |
+                       / \    |
+                             ===
+                    ");
+                    break;
+                default:
+                    Console.WriteLine(@"
+                        +---+
+                              |
+                              |
+                              |
+                             ===
+                    ");
+                    break;
+            }
+        }
+    }
+}
